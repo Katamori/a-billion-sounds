6a435704bd81a337585799143123d567af7be809
Katamori <zoltanschmidt@protonmail.com>
2020-05-10 17:45:19 +0200

    commit every temporary file I didn't dare upoading
    Author:    Katamori <zoltanschmidt@protonmail.com>

---
+++ notes/script/misc.md
@@ -0,0 +1,7 @@
+# Speedracer
+
+It's not always fun, but no one gives a fuck about that part.
+
+# Unassigned
+
+Ah...the Void Shard club...a sanctuary within Sanctuary.
---
+++ src/world/world.js
@@ -0,0 +1,9 @@
+const mapGameStates = {
+	phone: {
+		canUse: true,
+		hasWireless: true,
+		canCheckNotes: true,
+		// analog photo renderer
+		hasRenderer: false
+	}
+};
