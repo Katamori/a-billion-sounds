4c598b85e5b1ff48179fd6e5e64de2553f6569f2
Katamori <zoltanschmidt@protonmail.com>
2020-05-11 03:20:11 +0200

    remove addons and everything Tiled + move assets
    Author:    Katamori <zoltanschmidt@protonmail.com>

--- projectFiles/sprites_64.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.3.3" name="sprites_64" tilewidth="128" tileheight="128" tilecount="16" columns="4">
- <image source="../gfx/sprites_64.png" width="512" height="512"/>
-</tileset>
